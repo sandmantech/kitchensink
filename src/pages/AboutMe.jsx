bab99249cc29604a83c54e36540b43ebd9548f86
Sandeep Jayendran <sandeep@zelt.app>
2025-06-08 23:56:13 +0100

    update fix tiptap editor

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,13 +1,11 @@
-import React from 'react';
-import './AboutMe.css';
+import React from "react";
+import "./AboutMe.css";
 
 function AboutMe() {
   return (
     <div className="about-me-container">
       <h1>About Me</h1>
-      <p>Hello! I'm [Your Name/Nickname], and this is my personal page.</p>
-      <p>I'm interested in [Your Interests/Skills].</p>
-      <p>Welcome to my corner of the web!</p>
+      <p>Sandman here...experimenting.</p>
     </div>
   );
 }
